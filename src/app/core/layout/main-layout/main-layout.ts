fd95cd9830e4d2b99e3b9ca93d9128f80fe43ca1
Eid Aden <e-aden@hotmail.de>
2026-07-22 00:04:16 +0200

    Portfolio Photo added

--- src/app/core/layout/main-layout/main-layout.ts
+++ src/app/core/layout/main-layout/main-layout.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
@@ -11,7 +11,29 @@ import { UiService } from '../../services/ui.service';
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
-export class MainLayout {
+export class MainLayout implements AfterViewInit {
   uiService = inject(UiService);
   isMenuOpen = false;
+
+  ngAfterViewInit(): void {
+    const cursor = document.querySelector<HTMLElement>('.custom-cursor');
+
+    if (!cursor) return;
+
+    document.addEventListener('mousemove', (event: MouseEvent) => {
+      cursor.style.transform = `translate3d(
+        ${event.clientX}px,
+        ${event.clientY}px,
+        0
+      )`;
+    });
+
+    document.addEventListener('mouseleave', () => {
+      cursor.classList.add('custom-cursor--hidden');
+    });
+
+    document.addEventListener('mouseenter', () => {
+      cursor.classList.remove('custom-cursor--hidden');
+    });
+  }
 }
