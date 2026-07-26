13dd188354f178327878b11968693028c76c93d9
Eid Aden <e-aden@hotmail.de>
2026-07-26 14:00:59 +0200

    privacy upaded

--- src/app/features/home/sections/contact/contact.ts
+++ src/app/features/home/sections/contact/contact.ts
@@ -1,25 +1,27 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Buttons } from '../../../../shared/ui/buttons/buttons';
 import { TranslocoPipe } from '@jsverse/transloco';
 
 @Component({
   selector: 'app-contact',
-  imports: [FormsModule, Buttons, TranslocoPipe],
+  imports: [FormsModule, Buttons, TranslocoPipe, ReactiveFormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
 export class Contact {
-  http = inject(HttpClient);
-  borderlight = '2px solid #BBBBBB solid 2px';
+  private http = inject(HttpClient);
+  private fb = inject(FormBuilder);
 
-  contactForm = {
-    name: '',
-    email: '',
-    message: '',
-    privacy: false,
-  };
+  borderlight = '2px solid #BBBBBB';
+
+  contactForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', [Validators.required, Validators.minLength(10)]],
+    privacy: [false, Validators.requiredTrue],
+  });
 
   mailTest = false;
 
@@ -27,27 +29,46 @@ export class Contact {
     endPoint: 'https://eidaden.com/php/sendMail.php',
   };
 
-  onSubmit(ngForm: NgForm) {
-    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
-      this.http
-        .post(this.post.endPoint, this.contactForm, {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          responseType: 'text',
-        })
-        .subscribe({
-          next: (response) => {
-            console.log('Mail sent:', response);
-            ngForm.resetForm();
-          },
-          error: (error) => {
-            console.error('Error sending mail:', error);
-          },
-          complete: () => console.info('send post complete'),
-        });
-    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
-      ngForm.resetForm();
+  onSubmit(): void {
+    if (this.contactForm.invalid) {
+      this.contactForm.markAllAsTouched();
+      return;
     }
+
+    if (this.mailTest) {
+      this.resetForm();
+      return;
+    }
+
+    const formData = this.contactForm.getRawValue();
+
+    this.http
+      .post(this.post.endPoint, formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        responseType: 'text',
+      })
+      .subscribe({
+        next: (response) => {
+          console.log('Mail sent:', response);
+          this.resetForm();
+        },
+        error: (error) => {
+          console.error('Error sending mail:', error);
+        },
+        complete: () => {
+          console.info('Send post complete');
+        },
+      });
+  }
+
+  private resetForm(): void {
+    this.contactForm.reset({
+      name: '',
+      email: '',
+      message: '',
+      privacy: false,
+    });
   }
 }
